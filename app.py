d0a0b56d9a558a0e989c849ab0d3dccbefbb60db
Dhruv6227 <dhruvsharma8359@gmail.com>
2026-04-07 23:37:22 +0530

    ui

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pickle
+
+# load model
+model = pickle.load(open("model/model.pkl","rb"))
+vectorizer = pickle.load(open("model/vectorizer.pkl","rb"))
+
+# UI
+st.title("📧 Spam Email Classifier")
+st.write("Enter email text below to check whether it is Spam or Not Spam")
+
+email_text = st.text_area("Enter Email")
+
+if st.button("Predict"):
+
+    if email_text.strip() == "":
+        st.warning("Please enter some text")
+    
+    else:
+        email_vector = vectorizer.transform([email_text])
+        
+        prediction = model.predict(email_vector)
+
+        if prediction[0] == 1:
+            st.error("🚨 This is SPAM mail")
+        else:
+            st.success("✅ This is NOT spam mail")
